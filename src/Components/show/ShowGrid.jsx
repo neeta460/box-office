0371c50ce45e10e9b570d18c3c73245395731127
neeta <neetayadav3@gmail.com>
2021-12-01 12:10:06 +0530

    styled card

--- src/Components/show/ShowGrid.jsx
+++ src/Components/show/ShowGrid.jsx
@@ -1,22 +1,23 @@
+/* eslint-disable */
 import React from 'react'
 import ShowCard from './ShowCard'
+import {FlexGrid} from '../styled'
 import IMAGE_NOT_FOUND from '../../images/NotFound.png'
 
-const ShowGrid = ({data}) => {
-    <div>
+const ShowGrid = ({ data }) => {
+  return (
+    <FlexGrid>
+      {data.map(({ show }) => (
+        <ShowCard
+          key={show.id}
+          id={show.id}
+          name={show.name}
+          image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
+          summary={show.summary}
+        />
+      ))}
+    </FlexGrid>
+  );
+};
 
-    {
-      
-      data.map(({show}) => (
-      <ShowCard key={show.id} 
-      id={show.id} 
-      name={show.name} 
-      image={show.image ? show.image.medium : IMAGE_NOT_FOUND}
-      summary={show.summary}
-      />))
-    }
-    </div>
-  
-}
-
-export default ShowGrid
+export default ShowGrid;
