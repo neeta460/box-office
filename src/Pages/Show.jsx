1abd3c08045f47819f327467623a713876ce402d
neeta <neetayadav3@gmail.com>
2021-12-10 09:53:38 +0530

    created custom hooks

--- src/Pages/Show.jsx
+++ src/Pages/Show.jsx
@@ -6,6 +6,7 @@ import ShowMainData from '../Components/show/ShowMainData';
 import Seasons from '../Components/show/Seasons';
 import Cast from '../Components/show/Cast';
 import Details from '../Components/show/Details';
+import { InfoBlock, ShowPageWrapper } from './Show.styled';
 
 
 
@@ -68,7 +69,7 @@ const Show = () => {
   }
 
   return (
-    <div>
+    <ShowPageWrapper>
       <ShowMainData
         image={show.image}
         name={show.name}
@@ -77,25 +78,25 @@ const Show = () => {
         tags={show.genres}
       />
 
-      <div>
+      <InfoBlock>
         <h2>Details</h2>
         <Details
           status={show.status}
           network={show.network}
           premiered={show.premiered}
         />
-      </div>
+      </InfoBlock>
 
-      <div>
+      <InfoBlock>
         <h2>Seasons</h2>
         <Seasons seasons={show._embedded.seasons} />
-      </div>
+      </InfoBlock>
 
-      <div>
+      <InfoBlock>
         <h2>Cast</h2>
         <Cast cast={show._embedded.cast} />
-      </div>
-    </div>
+      </InfoBlock>
+    </ShowPageWrapper>
   );
 };
 
